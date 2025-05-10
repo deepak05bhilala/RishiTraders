cf6a6dee3f57190657d783d1a3b3e27f1c87307b
deepak bhilala <deepak05bhilala@gmail.com>
2025-05-10 17:02:30 +0530

    first commit

---
+++ src/main/java/com/example/RishiTraders/LldImplentationApplication.java
@@ -0,0 +1,18 @@
+package com.example.RishiTraders;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class LldImplentationApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LldImplentationApplication.class, args);
+
+	}
+
+	};
+
---
+++ src/test/java/com/example/LldImplementation/LldImplementationTests.java
@@ -0,0 +1,12 @@
+package com.example.LldImplementation;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class LldImplementationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
