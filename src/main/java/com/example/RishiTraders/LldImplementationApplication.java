6a8b2e80c115551fac73cac810796208eca8629b
Siddharth.Khandelwal <siddharth.khandelwal@alshaya.com>
2025-05-10 20:11:19 +0530

    Database Files added

---
+++ src/main/java/com/example/RishiTraders/LldImplementationApplication.java
@@ -0,0 +1,20 @@
+package com.example.RishiTraders;
+
+import Config.YamlPropertySourceFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+
+@SpringBootApplication
+@PropertySource(value = "classpath:persistence.yaml", factory = YamlPropertySourceFactory.class)
+public class LldImplementationApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(LldImplementationApplication.class, args);
+
+	}
+
+	};
+
